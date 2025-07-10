74c6a29690c3acdbb7849ff6b79e8ab0080546bb
akansha <85011592+AKANSHA978@users.noreply.github.com>
2025-07-10 11:17:08 +0530

    Update Ratesndrules.java

--- Page object model project/src/test/java/tests/Ratesndrules.java
+++ Page object model project/src/test/java/tests/Ratesndrules.java
@@ -23,8 +23,8 @@ public class Ratesndrules {
 
 	driver.get("https://recpalapp.co.uk/");
 	driver.findElement(By.xpath("//button[normalize-space()='Admin Login']")).click();
-	driver.findElement(By.id("inputEmail")).sendKeys("abck@gmail.com");
-	driver.findElement(By.id("inputPassword")).sendKeys("merchant@123");
+	driver.findElement(By.id("inputEmail")).sendKeys("anshika@gmail.com");
+	driver.findElement(By.id("inputPassword")).sendKeys("anshika@123");
 	driver.findElement(By.xpath("//button[@type='submit']")).click();
 	Thread.sleep(3000);
 		
