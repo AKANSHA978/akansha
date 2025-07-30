a9834491152f9d9c08048845f6011ba645e2009b
akansha <85011592+AKANSHA978@users.noreply.github.com>
2025-07-30 15:58:46 +0530

    Update Automationakansha.java

--- Automation dummy projects/Automationakansha.java
+++ Automation dummy projects/Automationakansha.java
@@ -1,5 +1,11 @@
 package Testfile;
 
 public class Automationakansha {
+  public static void main(String[]args)
+  {
+    driver.get("https://www.automationexercise.com/");
+    driver.manage().window().maximize();
+    driver.close();
+    
 
 }
